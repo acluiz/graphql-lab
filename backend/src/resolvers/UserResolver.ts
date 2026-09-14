337aab3423d4ff65016fe1cdf6a2a5c076562a14
Ana Luiz <acluiz.study@gmail.com>
2026-09-13 23:29:04 -0300

    feat: first schema and mutation

--- backend/src/resolvers/UserResolver.ts
+++ backend/src/resolvers/UserResolver.ts
@@ -1,9 +1,26 @@
-import { Query, Resolver } from "type-graphql";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import crypto from "crypto";
+
+import { User } from "../models/User.js";
 
 @Resolver()
 export class UserResolver {
-  @Query(() => String)
-  async hello(): Promise<string> {
-    return "Hello world";
+  private data: User[] = [];
+
+  @Query(() => [User])
+  async users(): Promise<User[]> {
+    return this.data;
+  }
+
+  @Mutation(() => User)
+  async createUser(@Arg("name", () => String) name: string): Promise<User> {
+    const user = {
+      id: crypto.randomUUID(),
+      name: name,
+    };
+
+    this.data.push(user);
+
+    return user;
   }
 }
